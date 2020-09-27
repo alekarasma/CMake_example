08d517a39d91598248b7e3c85a735fcead11986f
Asma <asmaa@questps.com.au>
2020-09-27 10:25:14 +1000

    Changed first program to new folder CMake_Lesson1

---
+++ CMake_Lesson1/build/CMakeFiles/crypto_test.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-kernel-if.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-md.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-kdf.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-utils.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-asym.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-aead.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-kpp.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-sym.c.o"
+  "CMakeFiles/crypto_test.dir/crypto_lib_src/Src/kcapi-rng.c.o"
+  "CMakeFiles/crypto_test.dir/project_src/main.c.o"
+  "crypto_test.pdb"
+  "crypto_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/crypto_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
