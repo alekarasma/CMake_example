97c9f63fc51db70d4315ce356602449b0d3aa813
Asma <asmaa@questps.com.au>
2020-10-04 16:36:25 +1100

    Chap2 - Creating shared library using CMake

---
+++ CMake_Lesson2/build/CMakeFiles/testCrypto.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-kernel-if.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-md.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-kdf.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-utils.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-asym.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-aead.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-kpp.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-sym.c.o"
+  "CMakeFiles/testCrypto.dir/crypto_lib_src/Src/kcapi-rng.c.o"
+  "libtestCrypto.pdb"
+  "libtestCrypto.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testCrypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
